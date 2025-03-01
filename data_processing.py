4c218914c8ba615aebc858877b3dba51c5e71925
Klahadore <stringycheese@protonmail.com>
2025-03-01 12:24:26 -0500

    Create data_processing.py

---
+++ data_processing.py
@@ -0,0 +1,5 @@
+# Split pdfs into individual pages of pngs
+# Black and white, remove color channels.
+# normalize resolution to 1024, 1024, add padding
+# Split dataset into train and val
+# Labeling data with tools
